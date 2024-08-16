ca82a8c02d5ac9850513c71648583a74f9d66029
sirdav1d <ddavid.diniz@gmail.com>
2024-08-16 15:40:28 -0300

    feat: :sparkles: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+/** @format */
+
+import { Contact1 } from '@/components/Contact1';
+import { CTA1 } from '@/components/CTA1';
+import { Feature351 } from '@/components/Feature351';
+import { Feature493 } from '@/components/Feature493';
+import { Footer4 } from '@/components/Footer4';
+import { HeaderHero82 } from '@/components/HeaderHero82';
+import { Navbar2 } from '@/components/Navbar2';
+import logo from '@/assets/logo-wisecam.png';
 
 export default function Home() {
-  return (
-    <main >
-    <h2>Hello world</h2>
-    </main>
-  );
+	return (
+		<main className='mx-auto w-full overflow-x-hidden flex flex-col items-center justify-center'>
+			<Navbar2 logo={logo} />
+			<HeaderHero82 />
+			<Feature493 />
+			<Feature351 />
+			<CTA1 />
+			<Contact1 />
+			<Footer4 />
+		</main>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	presets: [
+		require('@relume_io/relume-tailwind'),
+		require('tailwindcss-animate'),
+	],
+	plugins: [],
 };
 export default config;
