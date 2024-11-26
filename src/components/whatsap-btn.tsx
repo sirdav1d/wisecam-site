1ae2120412818020173cb63bbecfb95137db81fe
David Diniz Dos Santos <ddavid.diniz@gmail.com>
2024-11-26 12:08:41 -0300

    refactor: :sparkles: "Wrap Whatsapp button with anchor tag and add attributes"

--- src/components/whatsap-btn.tsx
+++ src/components/whatsap-btn.tsx
@@ -8,13 +8,19 @@ import logo from '@/../public/logos--whatsapp-icon.png';
 
 export default function WhatsappBtn() {
 	return (
-		<div className='fixed cursor-pointer hover:scale-110 transition-all duration-200 ease-linear bottom-5 right-5 z-[999999999999999999999999999999]'>
-			<Image
-				src={logo}
-				alt='logo whatsapp'
-				width={48}
-				height={48}
-			/>
-		</div>
+		<a
+			target='_blank'
+			href='#'
+			referrerPolicy='no-referrer'
+			aria-label='botão de whatsapp'>
+			<div className='fixed cursor-pointer hover:scale-110 transition-all duration-200 ease-linear bottom-5 right-5 z-[999999999999999999999999999999]'>
+				<Image
+					src={logo}
+					alt='logo whatsapp'
+					width={48}
+					height={48}
+				/>
+			</div>
+		</a>
 	);
 }
